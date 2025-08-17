d730e4f8696516f571606f2ba36cd4befaf78407
RaphaelOkuyama <raphaelokuyama123@gmail.com>
2025-08-17 17:34:43 -0300

    Upload de arquivos no AdminJS

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -2,16 +2,17 @@ import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
-import { episodeResourceOptions } from "./episode";
+import { episodeResourceFeatures, episodeResourceOptions } from './episode'
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Course,
     options: courseResourceOptions
   },
-  {
+	{
     resource: Episode,
-    options: episodeResourceOptions
+    options: episodeResourceOptions,
+    features: episodeResourceFeatures
   },
   {
     resource: Category,
