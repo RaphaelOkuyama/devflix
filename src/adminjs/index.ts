42c597add0caedd643f132b9ca6b0723df563249
RaphaelOkuyama <raphaelokuyama123@gmail.com>
2025-08-18 14:02:08 -0300

    Refatorando as configurações do AdminJS

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -1,11 +1,12 @@
 import AdminJs from 'adminjs'
 import AdminJsExpress from '@adminjs/express'
 import AdminJsSequelize from '@adminjs/sequelize'
-import { sequelize } from '../database'
 import { adminJsResources } from './resources'
-import { Category, Course, Episode, User } from '../models'
-import bcrypt from 'bcrypt'
 import { locale } from './locale'
+import { dashboardOptions } from './dashboard'
+import { brandingOptions } from './branding'
+import { authtenticationOptions } from './authentication'
+import { sequelize } from '../database'
 
 AdminJs.registerAdapter(AdminJsSequelize)
 
@@ -13,62 +14,14 @@ export const adminJs = new AdminJs({
   databases: [sequelize],
   resources: adminJsResources,
   rootPath: '/admin',
-  dashboard: {
-    component: AdminJs.bundle('./components/Dashboard'),
-		handler: async (req, res, context) => {
-      const courses = await Course.count()
-      const episodes = await Episode.count()
-      const category = await Category.count()
-      const standardUsers = await User.count({ where: { role: 'user' } })
-
-      res.json({
-        'Cursos': courses,
-        'Episódios': episodes,
-        'Categorias': category,
-        'Usuários': standardUsers
-      })
-    },
-  },
+  dashboard: dashboardOptions,
   locale: locale,
-  branding: {
-    companyName: 'DevFlix',
-    logo: '/devflix.svg',
-    theme: {
-      colors: {
-        primary100: '#ff0043',
-	      primary80: '#ff1a57',
-	      primary60: '#ff3369',
-	      primary40: '#ff4d7c',
-		    primary20: '#ff668f',
-	      grey100: '#151515',
-	      grey80: '#333333',
-	      grey60: '#4d4d4d',
-	      grey40: '#666666',
-	      grey20: '#dddddd',
-	      filterBg: '#333333',
-	      accent: '#151515',
-	      hoverBg: '#151515',
-      }
-    }
-  }
+  branding: brandingOptions
 })
 
-export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(adminJs, {
-  authenticate: async (email, password) => {
-    const user = await User.findOne({ where: { email } })
-
-    if (user && user.role === 'admin') {
-      const matched = await bcrypt.compare(password, user.password)
-
-      if (matched) {
-        return user
-      }
-    }
-
-    return false
-  },
-  cookiePassword: 'senha-do-cookie'
-}, null, {
-	resave: false,
-	saveUninitialized: false
-})
+export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(
+  adminJs,
+  authtenticationOptions,
+  null,
+  { resave: false, saveUninitialized: false }
+)
