8946597923992fa4d52408ff5c7f67ea7b618fca
RaphaelOkuyama <raphaelokuyama123@gmail.com>
2025-08-20 15:59:29 -0300

    Middleware de autorização

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -6,5 +6,9 @@ export const jwtService = {
   signToken: (payload: string | object | Buffer, expiration: SignOptions['expiresIn']): string => {
     const options: SignOptions = { expiresIn: expiration }
     return jwt.sign(payload, secret, options)
+  },
+
+ verifyToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+    jwt.verify(token, secret, callbackfn)
   }
 }
